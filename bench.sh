fe8d8a0be86f7a10211289a1dcbfc55d484542a7
tambe <tambeb@gmail.com>
2021-02-26 19:55:29 -0500

    changed domain argument to category

--- bench.sh
+++ bench.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-node benchmark protocol=dns,dnstcp,doh,dot domain=ad,top target=blockerdns1,blockerdns2,blockerdns3,blockerdns4,nextdns1,nextdns2,cleanbrowsing1,cleanbrowsing2,adguard1,adguard2
+node benchmark protocol=dns,dnstcp,doh,dot category=ad,top target=blockerdns1,blockerdns2,blockerdns3,blockerdns4,nextdns1,nextdns2,cleanbrowsing1,cleanbrowsing2,adguard1,adguard2
